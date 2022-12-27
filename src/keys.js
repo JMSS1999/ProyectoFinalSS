d7b4cfcddab5768af8a46277ec0e7d75c52b4004
Jhandry <jhandry.santorum@unl.edu.ec>
2022-12-26 22:01:18 -0500

    bd

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,10 @@
 module.exports = {
     database: {
         connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: 'admin',
-        database: 'dbsportsnotes'
+        host:  process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || 'root',
+        database: process.env.DB_NAME ||'dbsportsnotes',
+        port: process.env.DB_PORT || 3306
     }
 };
