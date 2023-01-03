c191d1836be48ec302cdd8d26d7f2a8a8d116430
JhandrySantorum <66153405+JMSS1999@users.noreply.github.com>
2023-01-03 09:39:28 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
         connectionLimit: 10,
-        host:  process.env.DB_HOST || 'localhost',
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || 'root',
+        host:  process.env.DB_HOST || 'db4free.net',
+        user: process.env.DB_USER || 'jhandry',
+        password: process.env.DB_PASSWORD || 'jhandry47',
         database: process.env.DB_DATABASE ||'dbsportsnotes',
         port: process.env.DB_PORT || 3306
     }
